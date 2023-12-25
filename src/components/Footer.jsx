ebc0b37c6a10fc5e8b36a0b66a94d74241fa460b
roshita <roshita.shakya@s2023.tu-chemnitz.de>
2023-12-25 21:22:28 +0100

    remove source

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,6 @@ const Footer = () => {
   return (
     <footer className="w-full h-8 2xl:h-9 bg-[#262629] text-theme-white flex justify-center items-center shadow-[0px_-1px_10px_0px] shadow-[rgba(0,0,0,0.2)]">
       <span className="text-sm font-medium"><p>&copy; Copyright {currentYear} Roshita Shakya. {i18n.t('copyright_text')}</p></span>{' '}
-      <a
-        className="text-sm absolute right-2.5"
-        href="https://github.com/roshita02/my-portfolio"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {i18n.t('footer')}
-      </a>
     </footer>
   );
 };
